7f1394de77c22b21720bdd14cd7f85c84b9cc0b0
Andreas Kring <andreas.kring@gmail.com>
2024-07-12 08:45:09 +0200

    build: Keycloak client and user

--- keycloak/main.tf
+++ keycloak/main.tf
@@ -4,36 +4,87 @@ terraform {
   }
   required_providers {
     keycloak = {
-      source = "mrparkers/keycloak"
+      source  = "mrparkers/keycloak"
       version = "4.4.0"
     }
   }
 }
 
+# REST API credentials and URL
 variable "keycloak_client_id" {
-  type        = string
-  default     = "admin-cli"
+  type    = string
+  default = "admin-cli"
 }
 variable "keycloak_user" {
-  type        = string
-  default     = "admin"
+  type    = string
+  default = "admin"
 }
 variable "keycloak_password" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 variable "keycloak_url" {
-  type        = string
-  default     = "http://keycloak:8080"
+  type    = string
+  default = "http://keycloak:8080"
+}
+
+# Application client variables
+variable "app_client_id" {
+  type    = string
+  default = "app"
+}
+
+variable "create_user" {
+  type    = bool
+  default = false
+}
+
+variable "user_password" {
+  type      = string
+  sensitive = true
 }
 
 provider "keycloak" {
-    client_id     = var.keycloak_client_id
-    username      = var.keycloak_user
-    password      = var.keycloak_password
-    url           = var.keycloak_url
+  client_id = var.keycloak_client_id
+  username  = var.keycloak_user
+  password  = var.keycloak_password
+  url       = var.keycloak_url
 }
 
-resource "keycloak_realm" "realm" {
+# Application realm
+resource "keycloak_realm" "app" {
   realm = "app"
+}
+
+# Application client
+resource "keycloak_openid_client" "openid_client" {
+  realm_id              = keycloak_realm.app.id
+  client_id             = var.app_client_id
+  access_type           = "PUBLIC"
+  implicit_flow_enabled = true
+  direct_access_grants_enabled = true
+  #   service_accounts_enabled = false
+
+  valid_redirect_uris = [
+    "http://frontend/logout"
+  ]
+
+  # web_origins = []
+  #full_scope_allowed =
+}
+
+resource "keycloak_user" "bruce" {
+  count    = var.create_user ? 1 : 0
+  realm_id = keycloak_realm.app.id
+  username = "bruce"
+  enabled  = true
+
+  email      = "bruce@kung.fu"
+  first_name = "Bruce"
+  last_name  = "Lee"
+
+  initial_password {
+    value     = var.user_password
+    temporary = false
+  }
 }
