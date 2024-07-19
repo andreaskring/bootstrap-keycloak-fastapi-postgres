bff78a1fc78d8b6799276b9253ff2d6de8891489
Andreas Kring <andreas.kring@gmail.com>
2024-07-19 23:57:06 +0200

    Add HtmlWebpackPlugin

--- bootstrap/webpack.config.js
+++ bootstrap/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ]
 };
-
