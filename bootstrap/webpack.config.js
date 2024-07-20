9d1d3d13cd807aa99bb558760816b9a0059e5687
Andreas Kring <andreas.kring@gmail.com>
2024-07-20 10:56:51 +0200

    Copy main.html to dist folder

--- bootstrap/webpack.config.js
+++ bootstrap/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -10,6 +11,11 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' })
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/html", to: "." }
+      ],
+    }),
   ]
 };
