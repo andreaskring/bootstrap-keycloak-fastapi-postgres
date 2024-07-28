5cedbba722144de0cb3da1c66eb2c3f97f5038f0
Andreas Kring <andreas.kring@gmail.com>
2024-07-28 09:33:59 +0200

    refactor: remove redundant auth dependency

--- backend/backend/endpoints.py
+++ backend/backend/endpoints.py
@@ -15,13 +15,14 @@ def get_router(**kwargs) -> APIRouter:
 
     @router.get("/require/auth")
     def require_auth(token: dict[str, Any] = Depends(auth)) -> dict[str, Any]:
+        # Explicit auth dependency here instead of just that included via the router,
+        # since we need the token (as an example)
         return token
 
     @router.get("/categories")
     async def categories(
         db_session: AsyncSession = Depends(async_db_session),
         db_tables: FacadeDict[str, Table] = Depends(tables),
-        token: dict[str, Any] = Depends(auth),
     ) -> list[dict[str, str | int]]:
         stmt = select(db_tables["category"])
         result = await db_session.execute(stmt)
@@ -40,7 +41,6 @@ def get_router(**kwargs) -> APIRouter:
             cat_id: int,
             db_session: AsyncSession = Depends(async_db_session),
             db_tables: FacadeDict[str, Table] = Depends(tables),
-            token: dict[str, Any] = Depends(auth),
     ) -> list[dict]:
         return []
 
